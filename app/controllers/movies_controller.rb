26cf31a34333e13591597036816967b4123702b5
dev <dev1672004@gmail.com>
2025-10-07 16:10:44 +0530

    controller, basic views and basic crud for movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,49 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path, notice: "Movie was successfully deleted."
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:name, :description, :release_date, :director, :writer, :actor)
+  end
+end
