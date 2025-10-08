4530a05c47c93a7b439111875370a3569decbcb5
Harendra Rathore <Harendra.rathore133@gmail.com>
2025-10-08 13:02:59 +0530

    feat: Devise gem integration for User

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem "bootsnap", require: false
 gem "administrate"
 gem "administrate-field-active_storage"
 
+# Authentication
+gem "devise"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,10 @@
 class User < ApplicationRecord
+ 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-    has_secure_password
-    has_many :ratings, dependent: :destroy
-    has_many :comments, dependent: :destroy
+  has_many :ratings, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, length: { minimum: 8 }, 
-              format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/, 
-                        message: "must contain at least one lowercase letter, one uppercase letter, one digit, and one special character" },
-              allow_nil: true
-    validates :password_confirmation, presence: true, on: :create
+  validates :name, presence: true
 end
