abe454d409c2e1b10f340a8a89e466b25cca8c76
harish khileri <harishkhileri98@gmail.com>
2026-02-11 16:53:43 +0530

    hot fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 require 'faker'
+require 'open-uri'
+require 'securerandom'
 
 puts "🌱 Seeding started..."
 
@@ -6,6 +8,20 @@ Rating.destroy_all
 Review.destroy_all
 Movie.destroy_all
 Genre.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+
+20.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email.downcase,
+    password: "harishkhileri",
+    password_confirmation: "harishkhileri"
+  )
+end
+
+puts "✅ Users created"
 
 genres = [
   "Action", "Comedy", "Drama", "Horror",
@@ -18,24 +34,33 @@ end
 
 puts "✅ Genres created"
 
+users = User.all
+
 20.times do
   Movie.create!(
     title: Faker::Movie.title,
     description: Faker::Lorem.paragraph(sentence_count: 3),
     release_date: Faker::Date.between(from: 20.years.ago, to: Date.today),
-    poster_url: Faker::Internet.url(host: "example.com", path: "/poster.jpg"),
-    genre: Genre.all.sample
+    genre: Genre.all.sample,
+    user: users.sample
   )
 end
 
 puts "🎬 Movies created"
 
-users = User.all
-
-raise "No users found! Seed users first." if users.empty?
-
+# Attach posters
 Movie.find_each do |movie|
-  users.sample(rand(7..10)).each do |user|
+  next if movie.avatar.attached?
+
+  file = URI.open("https://picsum.photos/300/450?random=#{SecureRandom.uuid}")
+  movie.avatar.attach(io: file, filename: "poster.jpg", content_type: "image/jpeg")
+end
+
+puts "🖼 Posters attached"
+
+# Ratings & Reviews
+Movie.find_each do |movie|
+  users.sample(rand(2..5)).each do |user|
     Rating.create!(
       rating: rand(1..5),
       user: user,
@@ -52,22 +77,3 @@ end
 
 puts "⭐ Ratings & Reviews created"
 puts "🌱 Seeding completed!"
-
-
-# puts "Cleaning database..."
-
-# User.delete_all
-# AdminUser.delete_all
-
-# Faker::UniqueGenerator.clear
-
-# puts "Creating users..."
-
-# 20.times do
-#   User.create!(
-#     name: Faker::Name.name,
-#     email: Faker::Internet.unique.email.downcase,
-#     password: "harishkhileri",
-#     password_confirmation: "harishkhileri"
-#   )
-# end
