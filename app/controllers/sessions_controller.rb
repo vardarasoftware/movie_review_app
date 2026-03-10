a03e82c3a036c7644b38d69d36cd232d26b4c4e6
harish khileri <harishkhileri98@gmail.com>
2026-03-10 13:57:07 +0530

    give secrete token proper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-  # Google sends a POST request back, so we skip the authenticity token check for this action
   skip_before_action :verify_authenticity_token, only: :omniauth
 
   def omniauth
